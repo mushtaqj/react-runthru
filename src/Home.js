f2ad3b6baee996628ad98b5fe40e892e4a3390c8
Mushtaq Jameel <mushtaq.jameel@villvay.com>
2022-04-13 02:21:17 +0530

    useState hook

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,16 @@
+import { useState } from 'react';
+
 export default function Home() {
+
+  const [name, setName] = useState('mario');
+
+  const handleClick = (name) => (setName(name));
+
   return (
     <div className="home">
       <h2>Homepage</h2>
+      <p>{name}</p>
+      <button onClick={() => handleClick("luigi")}>Click Me</button>
     </div>
-  )
+  );
 }
