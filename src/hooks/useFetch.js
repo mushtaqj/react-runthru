0b6dcc4fe9b5f7d81e8022b99b9802fd84a189ed
Mushtaq Jameel <mushtaq.jameel@villvay.com>
2022-04-13 15:26:19 +0530

    abort handler

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -6,22 +6,31 @@ export const useFetch = (url) => {
   const [error, setError] = useState(null);
 
   useEffect(() => {
-    fetch(url)
-      .then((res) => {
-        if (!res.ok) {
-          throw Error("Could not fetch the data for that resource");
-        }
-        return res.json();
-      })
-      .then((data) => {
-        setData(data);
-        setIsPending(false);
-        setError(null);
-      })
-      .catch(({ message }) => {
-        setError(message);
-        setIsPending(false);
-      });
+    // cancels inflight fetches
+    const abortController = new AbortController();
+
+    setTimeout(() => {
+      fetch(url, { signal: abortController.signal })
+        .then((res) => {
+          if (!res.ok) {
+            throw Error("Could not fetch the data for that resource");
+          }
+          return res.json();
+        })
+        .then((data) => {
+          setData(data);
+          setIsPending(false);
+          setError(null);
+        })
+        .catch(({ name, message }) => {
+          if (name !== "AbortError") {
+            setError(message);
+            setIsPending(false);
+          }
+        });
+    }, 1000);
+
+    return () => abortController.abort();
   }, [url]);
 
   return {
