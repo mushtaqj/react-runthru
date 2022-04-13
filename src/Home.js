c267d4e343fa0557ffb6d5022bc5df94fb640a38
Mushtaq Jameel <mushtaq.jameel@villvay.com>
2022-04-13 13:59:48 +0530

    add loading and erro handling

--- src/Home.js
+++ src/Home.js
@@ -5,6 +5,7 @@ import BlogList from "./components/BlogList";
 export default function Home() {
   const [blogs, setBlogs] = useState(null);
   const [isPending, setIsPending] = useState(true);
+  const [error, setError] = useState(null);
 
   const handleDelete = (blogId) => {
     const newBlogs = blogs.filter((blog) => blog.id !== blogId);
@@ -14,15 +15,26 @@ export default function Home() {
   // Run every time a component is rendered
   useEffect(() => {
     fetch(getBlogs)
-      .then((res) => res.json())
+      .then((res) => {
+        if (!res.ok) {
+          throw Error("Could not fetch the data for that resource");
+        }
+        return res.json();
+      })
       .then((blogs) => {
         setBlogs(blogs);
+        setIsPending(false);
+        setError(null);
+      })
+      .catch(({ message }) => {
+        setError(message);
         setIsPending(false);
       });
   }, []);
 
   return (
     <div className="home">
+      {error && <div>{error}</div>}
       {isPending && <div>Loading ...</div>}
       <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete} />
     </div>
