32f3c94e4daf3335aab1866a22f48823481c2243
Mushtaq Jameel <mushtaq.jameel@villvay.com>
2022-04-13 14:44:55 +0530

    add routes and create blog

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import CreateBlog from "./components/CreateBlog";
 import Home from "./components/Home";
 import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-      <div className="content">
-        <Home />
+    <Router>
+      <div className="App">
+        <Navbar />
+        <div className="content">
+          {/* Router logic */}
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/create" element={<CreateBlog />} />
+          </Routes>
+        </div>
       </div>
-    </div>
+    </Router>
   );
 }
 
